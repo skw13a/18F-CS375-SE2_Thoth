b9684bc6f205d90d5e43d018c4863909999217fd
skw13a <skw13a@acu.edu>
2018-12-04 09:59:06 -0600

    update current

--- assignments/Sedgewick/Huffman.java
+++ assignments/Sedgewick/Huffman.java
@@ -1,178 +1,193 @@
 /*************************************************************************
- *  Compilation:  javac Huffman.java
- *  Execution:    java Huffman - < input.txt   (compress)
- *  Execution:    java Huffman + < input.txt   (expand)
- *  Dependencies: BinaryIn.java BinaryOut.java
- *  Data files:   http://algs4.cs.princeton.edu/55compression/abra.txt
- *                http://algs4.cs.princeton.edu/55compression/tinytinyTale.txt
- *
- *  Compress or expand a binary input stream using the Huffman algorithm.
- *
- *  % java Huffman - < abra.txt | java BinaryDump 60
- *  010100000100101000100010010000110100001101010100101010000100
- *  000000000000000000000000000110001111100101101000111110010100
- *  120 bits
- *
- *  % java Huffman - < abra.txt | java Huffman +
- *  ABRACADABRA!
- *
- *************************************************************************/
+* Compilation: javac Huffman.java
+* Execution: java Huffman - < input.txt (compress)
+* Execution: java Huffman + < input.txt (expand)
+* Dependencies: BinaryIn.java BinaryOut.java
+* Data files: http://algs4.cs.princeton.edu/55compression/abra.txt
+* http://algs4.cs.princeton.edu/55compression/tinytinyTale.txt
+*
+* Compress or expand a binary input stream using the Huffman algorithm.
+*
+* % java Huffman - < abra.txt | java BinaryDump 60
+* 010100000100101000100010010000110100001101010100101010000100
+* 000000000000000000000000000110001111100101101000111110010100
+* 120 bits
+*
+* % java Huffman - < abra.txt | java Huffman +
+* ABRACADABRA!
+*
+*************************************************************************/
 
 public class Huffman {
 
-    // alphabet size of extended ASCII
-    private static final int R = 256;
+// alphabet size of extended ASCII
+private static final int R = 256;
 
-    // Huffman trie node
-    private static class Node implements Comparable<Node> {
-        private final char ch;
-        private final int freq;
-        private final Node left, right;
+// Huffman trie node
+private static class Node implements Comparable<Node> {
+private final char ch;
+private final int freq;
+private final Node left, right;
 
-        Node(char ch, int freq, Node left, Node right) {
-            this.ch    = ch;
-            this.freq  = freq;
-            this.left  = left;
-            this.right = right;
-        }
+Node(char ch, int freq, Node left, Node right) {
+this.ch = ch;
+this.freq = freq;
+this.left = left;
+this.right = right;
+}
 
-        // is the node a leaf node?
-        private boolean isLeaf() {
-            assert (left == null && right == null) || (left != null && right != null);
-            return (left == null && right == null);
-        }
+// is the node a leaf node?
+private boolean isLeaf() {
+assert (left == null && right == null) || (left != null && right != null);
+return (left == null && right == null);
+}
 
-        // compare, based on frequency
-        public int compareTo(Node that) {
-            return this.freq - that.freq;
-        }
-    }
-
-
-    // compress bytes from standard input and write to standard output
-    public static void compress() {
-        // read the input
-        String s = BinaryStdIn.readString();
-        char[] input = s.toCharArray();
-
-        // tabulate frequency counts
-        int[] freq = new int[R];
-        for (int i = 0; i < input.length; i++)
-            freq[input[i]]++;
-
-        // build Huffman trie
-        Node root = buildTrie(freq);
-
-        // build code table
-        String[] st = new String[R];
-        buildCode(st, root, "");
-
-        // print trie for decoder
-        writeTrie(root);
-
-        // print number of bytes in original uncompressed message
-        BinaryStdOut.write(input.length);
-
-        // use Huffman code to encode input
-        for (int i = 0; i < input.length; i++) {
-            String code = st[input[i]];
-            for (int j = 0; j < code.length(); j++) {
-                if (code.charAt(j) == '0') {
-                    BinaryStdOut.write(false);
-                }
-                else if (code.charAt(j) == '1') {
-                    BinaryStdOut.write(true);
-                }
-                else throw new RuntimeException("Illegal state");
-            }
-        }
-
-        // flush output stream
-        BinaryStdOut.flush();
-    }
-
-    // build the Huffman trie given frequencies
-    private static Node buildTrie(int[] freq) {
-
-        // initialze priority queue with singleton trees
-        MinPQ<Node> pq = new MinPQ<Node>();
-        for (char i = 0; i < R; i++)
-            if (freq[i] > 0)
-                pq.insert(new Node(i, freq[i], null, null));
-
-        // merge two smallest trees
-        while (pq.size() > 1) {
-            Node left  = pq.delMin();
-            Node right = pq.delMin();
-            Node parent = new Node('\0', left.freq + right.freq, left, right);
-            pq.insert(parent);
-        }
-        return pq.delMin();
-    }
+// compare, based on frequency
+public int compareTo(Node that) {
+return this.freq - that.freq;
+}
+}
 
 
-    // write bitstring-encoded trie to standard output
-    private static void writeTrie(Node x) {
-        if (x.isLeaf()) {
-            BinaryStdOut.write(true);
-            BinaryStdOut.write(x.ch);
-            return;
-        }
-        BinaryStdOut.write(false);
-        writeTrie(x.left);
-        writeTrie(x.right);
-    }
+// compress bytes from standard input and write to standard output
+public static void compress() {
+// read the input
+String s = BinaryStdIn.readString();
+char[] input = s.toCharArray();
 
-    // make a lookup table from symbols and their encodings
-    private static void buildCode(String[] st, Node x, String s) {
-        if (!x.isLeaf()) {
-            buildCode(st, x.left,  s + '0');
-            buildCode(st, x.right, s + '1');
-        }
-        else {
-            st[x.ch] = s;
-        }
-    }
+// tabulate frequency counts
+int[] freq = new int[R];
+for (int i = 0; i < input.length; i++)
+freq[input[i]]++;
+
+// build Huffman trie
+Node root = buildTrie(freq);
+
+// build code table
+String[] st = new String[R];
+buildCode(st, root, "");
+
+// print trie for decoder
+writeTrie(root);
+System.err.println("writeTrie");
+
+// print number of bytes in original uncompressed message
+BinaryStdOut.write(input.length);
+System.err.println("writing input length " + input.length);
+
+System.err.println("encoding... ");
+// use Huffman code to encode input
+for (int i = 0; i < input.length; i++) {
+String code = st[input[i]];
+System.err.print("Char " + input[i] + " ");
+for (int j = 0; j < code.length(); j++) {
+if (code.charAt(j) == '0') {
+BinaryStdOut.write(false);
+System.err.print("0");
+}
+else if (code.charAt(j) == '1') {
+BinaryStdOut.write(true);
+System.err.print("1");
+}
+else throw new RuntimeException("Illegal state");
+}
+System.err.println("");
+}
+
+// flush output stream
+BinaryStdOut.flush();
+}
+
+// build the Huffman trie given frequencies
+private static Node buildTrie(int[] freq) {
+
+// initialze priority queue with singleton trees
+MinPQ<Node> pq = new MinPQ<Node>();
+for (char i = 0; i < R; i++)
+if (freq[i] > 0)
+pq.insert(new Node(i, freq[i], null, null));
+
+// merge two smallest trees
+while (pq.size() > 1) {
+Node left = pq.delMin();
+Node right = pq.delMin();
+Node parent = new Node('\0', left.freq + right.freq, left, right);
+System.err.println("buildTrie parent " + left.freq + " " + right.freq);
+pq.insert(parent);
+}
+return pq.delMin();
+}
 
 
-    // expand Huffman-encoded input from standard input and write to standard output
-    public static void expand() {
+// write bitstring-encoded trie to standard output
+private static void writeTrie(Node x) {
+if (x.isLeaf()) {
+BinaryStdOut.write(true);
+BinaryStdOut.write(x.ch);
+System.err.println("T" + x.ch);
+return;
+}
+BinaryStdOut.write(false);
+System.err.print("F");
 
-        // read in Huffman trie from input stream
-        Node root = readTrie(); 
+writeTrie(x.left);
+writeTrie(x.right);
+}
 
-        // number of bytes to write
-        int length = BinaryStdIn.readInt();
-
-        // decode using the Huffman trie
-        for (int i = 0; i < length; i++) {
-            Node x = root;
-            while (!x.isLeaf()) {
-                boolean bit = BinaryStdIn.readBoolean();
-                if (bit) x = x.right;
-                else     x = x.left;
-            }
-            BinaryStdOut.write(x.ch);
-        }
-        BinaryStdOut.flush();
-    }
-
-
-    private static Node readTrie() {
-        boolean isLeaf = BinaryStdIn.readBoolean();
-        if (isLeaf) {
-            return new Node(BinaryStdIn.readChar(), -1, null, null);
-        }
-        else {
-            return new Node('\0', -1, readTrie(), readTrie());
-        }
-    }
+// make a lookup table from symbols and their encodings
+private static void buildCode(String[] st, Node x, String s) {
+if (!x.isLeaf()) {
+buildCode(st, x.left, s + '0');
+buildCode(st, x.right, s + '1');
+}
+else {
+st[x.ch] = s;
+System.err.println("buildCode " + x.ch + " " + s);
+}
+}
 
 
-    public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
-        else if (args[0].equals("+")) expand();
-        else throw new RuntimeException("Illegal command line argument");
-    }
+// expand Huffman-encoded input from standard input and write to standard output
+public static void expand() {
+
+// read in Huffman trie from input stream
+Node root = readTrie();
+
+// number of bytes to write
+int length = BinaryStdIn.readInt();
+
+// decode using the Huffman trie
+for (int i = 0; i < length; i++) {
+Node x = root;
+while (!x.isLeaf()) {
+boolean bit = BinaryStdIn.readBoolean();
+if (bit) x = x.right;
+else x = x.left;
+}
+BinaryStdOut.write(x.ch);
+}
+BinaryStdOut.flush();
+}
+
+
+private static Node readTrie() {
+boolean isLeaf = BinaryStdIn.readBoolean();
+if (isLeaf) {
+char x = BinaryStdIn.readChar();
+System.err.println("t: " + x );
+return new Node(x, -1, null, null);
+}
+else {
+System.err.print("f");
+return new Node('\0', -1, readTrie(), readTrie());
+}
+}
+
+
+public static void main(String[] args) {
+if (args[0].equals("-")) compress();
+else if (args[0].equals("+")) expand();
+else throw new RuntimeException("Illegal command line argument");
+}
 
 }
